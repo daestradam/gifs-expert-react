94104841920e23a21622bc2222c17c2c6dc7b300
Daniel <daestradam@unal.edu.co>
2024-06-26 22:24:49 -0500

    newCategory.toUpperCase()

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,9 +3,11 @@ import {AddCategory} from './components/AddCategory'
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories, setCategories] = useState([]);
     const onAddCategory = (newCategory) => {
-        setCategories([ ...categories, newCategory ]);
+        const category = newCategory.toUpperCase();
+        if( categories.includes(category) ) return;
+        setCategories([ ...categories, category ]);
     };
 
     return (
