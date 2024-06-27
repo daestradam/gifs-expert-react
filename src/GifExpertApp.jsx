33f7078bc3bb68e508c4057b7d575c8b1bc4f6a0
Daniel <daestradam@unal.edu.co>
2024-06-27 00:37:54 -0500

    fetch api

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,32 +1,34 @@
-import {useState} from 'react';
-import {AddCategory} from './components/AddCategory'
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState([]);
     const onAddCategory = (newCategory) => {
         const category = newCategory.toUpperCase();
-        if( categories.includes(category) ) return;
-        setCategories([ ...categories, category ]);
+        if (categories.includes(category)) return;
+        setCategories([...categories, category]);
     };
 
     return (
         <>
-            {/* titulo */}
+
             <h1>GifExpertApp</h1>
 
-            {/* input */}
-            <AddCategory 
+            <AddCategory
                 onNewCategory={onAddCategory}
             />
 
-            {/* listado de Gif */}
-            <ol>
-                {categories.map(category => {
-                        return <li key={category}>{category}</li>
-                    })
-                }
-            </ol>
+            {
+                categories.map((category) => (
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+
         </>
     )
 }
