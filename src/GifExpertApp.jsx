7d2ea216871d936734a0f3809ac85ebd7942f2ee
Daniel <daestradam@unal.edu.co>
2024-06-26 21:30:56 -0500

    Agregar una nueva categoria a la lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,29 @@
+import {useState} from 'react';
+//import {AddCategory} from './components/AddCategory'
+
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>GifExpertApp</h1>
-    </>
-  )
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const onAddCategory = () => {
+        setCategories([...categories,'valorant']);
+    };
+
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* input */}
+            {/* <AddCategory /> */}
+
+            {/* listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                {categories.map(category => {
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
+        </>
+    )
 }
