5fee8cd38ef0069ea51b30da6b27134d2337c161
Daniel <daestradam@unal.edu.co>
2024-06-27 19:46:38 -0500

    useEffect no acepta promesas, image.title

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,18 +1,29 @@
 import { getGifs } from "../helpers/getGifs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const GifGrid = ({ category }) => {
 
+  const [images, setImages] = useState([]);
+
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  }
+
   useEffect( () => {
-    getGifs(category);
-    
-  }, []);
-
-
+    getImages();
+  }, [])
 
   return (
     <>
       <h3>{category}</h3>
+      <ol>
+        {
+          images.map( ({id, title}) => 
+            <li key={ id }>{ title }</li>
+          )
+        }
+      </ol>
     </>
   )
 }
