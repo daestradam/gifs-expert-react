b2bbb487394ef45b349adbca26279fbdf238536c
Daniel <daestradam@unal.edu.co>
2024-06-30 22:40:07 -0500

    test - mock, jest.fn()

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -14,9 +14,9 @@ describe('Prueba de <AddCategory/>', () => {
 
     test('debe de llamar onNewCategory si el input tiene un valor', () => { 
         const inputValue = 'Saitama';
-        //TODO: ????
+        const onNewCategory = jest.fn();
 
-        render(<AddCategory onNewCategory={()=> {}} />);
+        render(<AddCategory onNewCategory={onNewCategory} />);
         const input = screen.getByRole('textbox');
         const form = screen.getByRole('form');
 
@@ -24,6 +24,23 @@ describe('Prueba de <AddCategory/>', () => {
         fireEvent.submit(form);
 
         expect(input.value).toBe('');
+
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue);
+
+    })
+
+    test('no debe de llamar onNewCategory si el input está vacío', () => { 
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(onNewCategory).not.toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        
     })
 
 
